ac3126312c826740d4cd3054c3df53c445c40c6a
BPaetow <bernhardpaetow@gmail.com>
2018-11-13 19:35:01 -0600

    completed division and quotient

--- GitPractice/Calculator.cs
+++ GitPractice/Calculator.cs
@@ -6,5 +6,27 @@ namespace GitPractice
 {
     class Calculator
     {
+        public int Add(int num1, int num2) 
+            {
+            int sum = 0;
+            sum = num1 + num2;
+            return sum;
+            } 
+
+        public int Subtract(int num1, int num2)
+        {
+            int difference = 0;
+            difference = num1 - num2;
+            return difference;
+        }
+
+        public int Multiply(int num1, NetTcpStyleUriParser num2)
+        {
+            return 0;
+        }
+        public int Divide (int num1, in num2)
+        {
+            return 0;
+        }
     }
 }
